78b6d35b43eec04e7173340fd150d3cd06adf5f2
mazinmohd <mazenomermazen2002@gmail.com>
2026-06-17 01:23:41 +0200

    change database

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "petverse-db";
-$user = "root";
-$password = "root";
-$database = "pet_store";
+$host = "sql101.infinityfree.com";
+$user = "if0_42200143";
+$password = "Mazenomer2002";
+$database = "if0_42200143_pet_store";
 
 $conn = mysqli_connect(
     $host,
